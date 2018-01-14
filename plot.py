495d8a856d4b0a2566e1e36b17e6b8d06dc2771c
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2018-01-14 22:15:04 +0100

    added auswertung

--- plot.py
+++ plot.py
@@ -1,4 +1,44 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-from mathe import matheplot as mp
+from scipy.optimize import curve_fit
+import scipy.constants.constants as const
+from uncertainties import ufloat
+from uncertainties import unumpy
+
+#plot1
+mpl.use('pgf')
+mpl.rcParams.update({
+    'pgf.preamble': r'\usepackage{siunitx}',
+})
+
+t, y = np.genfromtxt('content/Messwerte1.txt', unpack=True)
+
+x= t/26320
+
+plt.xlabel(r'$t/\si{\second}$')
+plt.ylabel(r'$U/\si{\volt}$')
+plt.yscale('log')
+plt.grid(True, which='both')
+
+
+# Fitvorschrift
+def f(x, A, B):
+    return A*x + B      #jeweilige Fitfunktion auswaehlen:
+
+params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+for i in range(0, len(uparams)):
+    print(chr(ord('A') + i), "=" , uparams[i])
+print()
+
+lin = np.linspace(x[0], x[-1], 1000)
+plt.plot(lin, f(lin, *params), "xkcd:orange", label=r'Regression' )
+plt.plot(x, y, ".", color="xkcd:blue", label="Messwerte")
+
+
+
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/messung1.pdf')
+plt.clf()
