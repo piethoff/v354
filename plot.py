e1bb2a9b843e021c35a66149690b55ce3f678731
Piet Hoffmann <hoffmannpiet42@gmail.com>
2018-01-15 02:07:15 +0100

    durchführung, auswertung angefangen

--- plot.py
+++ plot.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+from mathe import matheplot as mp
 from scipy.optimize import curve_fit
-import scipy.constants.constants as const
 from uncertainties import ufloat
 from uncertainties import unumpy
 
-#plot1
 mpl.use('pgf')
 mpl.rcParams.update({
     'pgf.preamble': r'\usepackage{siunitx}',
@@ -42,3 +41,57 @@ plt.tight_layout()
 plt.legend()
 plt.savefig('build/messung1.pdf')
 plt.clf()
+
+plt.figure()
+plt.savefig("build/plot.pdf")
+
+data = np.genfromtxt("content/freq.txt", unpack=True)
+data[0] /= data[1]
+data[2] *= 2*np.pi
+
+
+def freq(w, L, C, R):
+    return 1 / np.sqrt((1-L*C*w**2)**2 + (w*R*C)**2)
+
+params, covar = curve_fit(freq, data[2], data[0], p0=(0.016, 0.000000002, 682))
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+
+print(uparams)
+
+plt.plot(data[2], data[0], ".", label="Messwerte")
+plt.xlabel(r"$\omega /\si[per-mode=reciprocal]{\second}$")
+plt.ylabel(r"$\frac{U_C}{U_0}$")
+#lin = np.linspace(np.amin(data[2]), np.amax(data[2]), 1000)
+#plt.plot(freq(lin, *params), label="Regression")
+plt.xscale("log")
+plt.grid(which="both")
+plt.legend()
+plt.tight_layout()
+plt.savefig("build/freq.pdf")
+
+plt.clf()
+
+data = np.genfromtxt("content/phase.txt", unpack=True)
+
+phi = np.array([2*np.pi*data[0] / data[1]])
+data = np.concatenate((data, phi))
+print(data[3])
+
+def phase(w, L, C, R):
+    return np.arctan((-w*R*C)/(1-L*C*w**2))
+
+params, covar = curve_fit(phase, data[2]*2*np.pi, data[3], p0=(0.016, 0.000000002, 682))
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+
+print(uparams)
+
+#lin = np.linspace(np.amin(data[2]), np.amax(data[2]), 1000)
+#plt.plot(lin, phase(lin*2*np.pi, *params), label="Regression")
+plt.plot(data[2], data[3], ".", label="Messdaten")
+plt.xlabel(r"$f/\si{\hertz}$")
+plt.ylabel(r"$\phi$")
+plt.xscale("log")
+plt.grid(which="both")
+plt.legend()
+plt.tight_layout()
+plt.savefig("build/phase.pdf")
